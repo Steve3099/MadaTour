3c853ba5af0aa6ab7267886fb76d4cd4e6a5c23e
sharatprox <61748362+rakotovaonytoky@users.noreply.github.com>
2023-07-29 21:22:10 +0300

    Revert "spash screen"
    This reverts commit 37e6b4868af2284c942efb70ebe9af600e5ba72d.

--- app/src/main/java/com/example/madatour/vue/MainActivity.java
+++ app/src/main/java/com/example/madatour/vue/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.madatour.R;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -45,12 +46,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static class HomeActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_home);
-        }
-    }
 }
