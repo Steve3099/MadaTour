9f25665caf1e64833bd0239dede7c875dae98220
sharatprox <sharat.seeboruth@proximity.fr>
2023-07-31 17:13:11 +0300

    add signup screen

--- app/src/main/java/com/example/madatour/vue/SignUpActivity.java
+++ app/src/main/java/com/example/madatour/vue/SignUpActivity.java
@@ -3,26 +3,30 @@ package com.example.madatour.vue;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.madatour.R;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
-    Button callSignup;
+
+    Button callLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_sign_up);
 
-        callSignup = findViewById(R.id.signup_screen);
-        callSignup.setOnClickListener(new View.OnClickListener() {
+        callLogin = findViewById(R.id.call_login);
+        callLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this,HomeActivity.class);
                 startActivity(intent);
             }
         });
