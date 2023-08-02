c64f74a4515df88e6f6fbe30c4ab67107bd2e767
Ny Toky <rakotovaonytaka@gmail.com>
2023-08-02 22:20:32 +0300

    [feature] home activity
    f

--- app/src/main/java/com/example/madatour/vue/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/madatour/vue/ui/home/HomeFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.madatour.R;
 import com.example.madatour.controler.CategoryAdapter;
@@ -20,6 +21,7 @@ import com.example.madatour.controler.TourismAdapter;
 import com.example.madatour.databinding.FragmentHomeBinding;
 import com.example.madatour.modele.Category;
 import com.example.madatour.modele.Tourism;
+import com.example.madatour.util.SpacesItemDecoration;
 
 import java.util.ArrayList;
 
@@ -61,12 +63,22 @@ public class HomeFragment extends Fragment {
     private void featuredTourismRecycler() {
         featuredTourismRecycler.setHasFixedSize(true);
         featuredTourismRecycler.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+        featuredTourismRecycler.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         ArrayList<Tourism> listTourism = new ArrayList<>();
         listTourism.add(new Tourism("1","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
         listTourism.add(new Tourism("2","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
         listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
+        listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
+        listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
+        listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
+        listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
+        listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
+        listTourism.add(new Tourism("3","Monuments","Les monuments historique à Madagascar",(R.drawable.lemurs),"Monuments"));
         adapter = new TourismAdapter(listTourism);
         featuredTourismRecycler.setAdapter(adapter);
+//        Add space in first list
+        SpacesItemDecoration decoration = new SpacesItemDecoration(20);
+        featuredTourismRecycler.addItemDecoration(decoration);
     }
     private void featuredCategoryRecycler() {
         featuredCategoryRecycler.setHasFixedSize(true);
